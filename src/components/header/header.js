991c70b8d6f52ba3d784003eb7d04f3ef626dc31
Patryk Urbanski <pat.urbanski@gmail.com>
2020-05-09 11:26:51 +0200

    rewrite header to react

--- src/components/header/header.js
+++ src/components/header/header.js
@@ -1,20 +1,33 @@
-import React from "react";
+import React, {useState} from "react";
 
 export const Header = () => {
+    const [openMenu, setOpenMenu] = useState("");
+
+    const handleOpenNav = (e) => {
+        e.preventDefault();
+        setOpenMenu("sidebarWidth")
+    };
+
+    const handleCloseNAv = (e) => {
+        e.preventDefault();
+        setOpenMenu("");
+    };
+
+
     return (
         <>
             <header className="header">
                 <nav className="header__nav container">
                     <img className="logo" src="../images/logo1.png" alt="logo" />
-                        <a href="#" className="menu-btn open">
+                        <a href="#" onClick={handleOpenNav} className={`menu-btn open`}>
                             <i className="fas fa-braille"></i>
                         </a>
-                        <div className="header__nav__list">
-                            <i id="closeBtn" className="fas fa-times visible"></i>
-                            <a href="#strategiesPage">Strategies</a>
-                            <a href="/journalApp.html">Journal App</a>
-                            <a href="developersStuff.html">Developer's stuff</a>
-                            <a href="#">Author</a>
+                        <div className={`header__nav__list ${openMenu}`}>
+                            <i id="closeBtn" onClick={handleCloseNAv} className={`fas fa-times visible`}></i>
+                            <a href="#" onClick={handleCloseNAv}>Strategies</a>
+                            <a href="#" onClick={handleCloseNAv}>Journal App</a>
+                            <a href="#" onClick={handleCloseNAv}>Developer's stuff</a>
+                            <a href="#" onClick={handleCloseNAv}>Author</a>
                         </div>
                 </nav>
             </header>
