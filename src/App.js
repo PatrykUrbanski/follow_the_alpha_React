250a1e8bba302654e17d09bc6cd4bde04ae92b8c
Patryk Urbanski <pat.urbanski@gmail.com>
2020-05-14 18:43:36 +0200

    change router type to work on github pages

--- src/App.js
+++ src/App.js
@@ -4,18 +4,18 @@ import "./sass/main.scss";
 import {MainTemplate} from "./components/main/mainTemplate";
 import {ParallaxProvider} from "react-scroll-parallax/cjs";
 import {Header} from "./components/header/header";
-import {BrowserRouter} from "react-router-dom";
+import {BrowserRouter, HashRouter} from "react-router-dom";
 import {LoadingScreen} from "./components/loadingScreen/loadingScreen";
 
 
 
 const App = () => {
     return (
-        <BrowserRouter>
+        <HashRouter>
         <ParallaxProvider>
             <MainTemplate/>
         </ParallaxProvider>
-        </BrowserRouter>
+        </HashRouter>
         )
 };
 
